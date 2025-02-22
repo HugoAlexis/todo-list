9e78ade1a3230a21cc04265b4d15413e1e534418
Hugo Alexis <hugoalexistorrespasillas@gmail.com>
2025-02-21 21:31:11 -0600

    Bug corrected in functions file, and say_goodbye function changed

--- functions.py
+++ functions.py
@@ -2,6 +2,6 @@
 def print_greeting(name):
     print('Hi ', name)
 
-def say_hello(name):
-    print('Hello ', name)
+def say_goodbye(name, goodbye_text='goodbye'):
+    print(goodbye_text, ' ', name)
 
