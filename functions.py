235500990342df04a83ad7e7a39e90a635fe38c7
Hugo Alexis <hugoalexistorrespasillas@gmail.com>
2025-02-22 21:45:43 -0600

    Documentation added to functions module

--- functions.py
+++ functions.py
@@ -1,7 +1,15 @@
 
 def print_greeting(name):
+    """
+    Prints out a greeting message for the person with name 'name' using
+    the standard output.
+    """
     print('Hi ', name)
 
 def say_goodbye(name, goodbye_text='goodbye'):
+    """
+    Prints out a goodbye message for the person with the name given
+    using the standard output.
+    """
     print(goodbye_text, ' ', name)
 
