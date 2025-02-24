8cd36219807390005ce44a2b01e5e847e6527269
Hugo Alexis <hugoalexistorrespasillas@gmail.com>
2025-02-23 20:36:09 -0600

    Adding a goodbye message at the end of main module.

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ if __name__ == '__main__':
     print('Hello world!')
     print_greeting('Hugo Alexis')
     say_goodbye('Hugo Alexis')
+    print('Bye bye')
+    print('I almost forgot to say goodbye')
+
 
